720b75f81146f7fb05563a7f5d7a5aa7be8883ea
Tanya Engelhart <tkengelhart@gmail.com>
2021-04-23 18:49:18 -0500

    working on capturing click values

--- client.js
+++ client.js
@@ -3,27 +3,23 @@ console.log('in JS');
 $(onReady);
 const emplArr = [];
 
-
-
 function onReady() {
+    firstName = $('#fName').val();
+    lastName = $('#lName').val();
+    idNumber = $('#idNum').val();
+    jobTitle = $('#jTitle').val();
+    annSalary = $('#aSalary').val();
+
     $('#submitButton').on('click', function (event) {
-        firstName = $('#fName').val();
-        lastName = $('#lName').val();
-        idNumber = $('#idNum').val();
-        jobTitle = $('#jTitle').val();
-        annSalary = $('#aSalary').val();
         firstName.empty();
         lastName.empty();
         idNumber.empty();
         jobTitle.empty();
         annSalary.empty();
-
-
         let table = $('<table></table');
         let tbody = $('<tbody id="tableBody"></tbody');
         table.append(tbody);
         $('.formContainer').append(table);
-
         for (let i = 0; i < emplArr.length; i++) {
             emplArr.push(emplArr[i]);
             const emplObj = {
@@ -38,6 +34,8 @@ function onReady() {
                 tbody.append(`<li>` + emplObj.first + emplObj.last + emplObj.id + emplObj.title + emplObj.salary + `</li>`);
             }
         }
+        console.log(emplArr);
+
 
     })
 };
