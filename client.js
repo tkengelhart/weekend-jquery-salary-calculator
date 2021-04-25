27f4f40f56651cbe4a83cc7fea615b024d326cb9
Tanya Engelhart <tkengelhart@gmail.com>
2021-04-24 19:55:13 -0500

    table formatting done, working on total

--- client.js
+++ client.js
@@ -1,46 +1,64 @@
-$(onReady);
 const emplArr = [];
+const monthlyTotal = 0;
+
 
 function onReady() {
-    $('#submitButton').on('click', function () {
-        //capture input box values and append to DOM
-        const firstName = $('#fName').val();
-        const lastName = $('#lName').val();
-        const idNumber = $('#idNum').val();
-        const jobTitle = $('#jTitle').val();
-        const annSalary = $('#aSalary').val();
-        const emplObj = {
+    // function that runs when onReady is called
+    $('#submitButton').on('click', function () { //clickhandler
+        const firstName = $('#fName').val(); //variable for first name input
+        const lastName = $('#lName').val(); //variable for last name input
+        const idNumber = $('#idNum').val(); //variable for id input
+        const jobTitle = $('#jTitle').val(); //variable for job title input
+        const annSalary = $('#aSalary').val(); //variable for salary input
+        const emplObj = {  //create employee object
             first: firstName,
             last: lastName,
             id: idNumber,
             title: jobTitle,
             salary: annSalary,
         };
-        emplArr.push(emplObj);
-        return emplArr;
-    });
+        emplArr.push(emplObj);  //push employee object to array
+        addEmployee(firstName, lastName, idNumber, jobTitle, annSalary)
+    });  //call add employee function to add employee to list
 
-    $('#employeeList').empty();
+    $('#employeeTable').empty();  //clear employees on initialize
 
-    for (let i = 0; i < emplArr.length; i++) {
-        addEmployee(emplyArr[i].first, emplyArr[i].last, emplyArr[i].id, emplyArr[i].title, emplyArr[i].salary);
+    for (let i = 0; i < emplArr.length; i++) {  //loop through array and populate employees
+        addEmployee(emplArr[i].first, emplArr[i].last, emplArr[i].id, emplArr[i].title,
+            emplArr[i].salary);
+        const total = function monthlyCosts(annSalary) {
+            monthlyTotal += emplArr[i].salary;
+            $('#monthlySum').append(`<hr>` + Monthly + Total + total`</hr>`);
+
+        }
 
     }
-    function addEmployee(first, last, id, title, salary) {
-        $('#employeeList').append(`<li>` + firstName + lastName + idNumber + jobTitle + annSalary + `<li>`);  //update this with rows later
-    }  //outside of on ready function call so it doesn't repeat
+    function addEmployee(firstName, lastName, idNumber, jobTitle, annSalary) { //create function to append to list
+        $('#employeeTable').append(`
+        <tr>
+            <td>
+            ${firstName}
+            </td>
+            <td>
+            ${lastName}
+            </td>
+            <td>
+            ${idNumber}
+            </td>
+            <td>
+            ${jobTitle}
+            </td>
+            <td>
+            ${annSalary}
+            </td>
+            <hr>
+        </tr>`)
 
-
-
-
-
-
-
+    };
 
 
 }
-$(onReady);
 
-    // function monthlyCosts() {  //will use for loop to capture annual salary of employees
-    //     annSalary[i];
-    // }
+
+
+$(onReady);
