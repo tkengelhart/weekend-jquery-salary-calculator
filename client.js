2bffb3d5a9b698ec89c82aa7ecbf84a3721f2d52
Tanya Engelhart <tkengelhart@gmail.com>
2021-04-25 14:21:35 -0500

    monthly calc working, added delete buttons

--- client.js
+++ client.js
@@ -1,4 +1,6 @@
 const emplArr = [];
+let sum = 0;
+
 
 function onReady() {
     // function that runs when onReady is called
@@ -7,14 +9,14 @@ function onReady() {
         const lastName = $('#lName').val(); //variable for last name input
         const idNumber = $('#idNum').val(); //variable for id input
         const jobTitle = $('#jTitle').val(); //variable for job title input
-        const annSalary = $('#aSalary').val(); //variable for salary input
+        const annSalary = Number($('#aSalary').val()); //variable for salary input
 
         const emplObj = {  //create employee object
             first: firstName,
             last: lastName,
             id: idNumber,
             title: jobTitle,
-            salary: annSalary,
+            salary: Number(annSalary),
         }; //end create object
 
         emplArr.push(emplObj);  //push employee object to array
@@ -22,11 +24,23 @@ function onReady() {
         $('#employeeTable').empty();  //clear employees on initialize
 
         for (let i = 0; i < emplArr.length; i++) {
-            let sum = 0;
-            const grandTotal = sum += emplArr[i].salary;
+            if (emplArr[i].salary > 0) {
+                sum += (Number(emplArr[i].salary))
+            };
+            addSum(sum);  //looping through to add salary to sum on each loop, make sure to empty sum after values entered (below)
+
             //loop through array and populate employees
             addEmployee(emplArr[i].first, emplArr[i].last, emplArr[i].id, emplArr[i].title,
                 emplArr[i].salary);
+
+
+            // let salary = parseInt(emplArr[i].salary);
+            // sum += salary;
+            // $('#monthlySum').append(sum);
+
+
+            // console.log(parseFloat(sum));
+
         }; //end for loop
 
         //empty input fields
@@ -35,11 +49,20 @@ function onReady() {
         $('#idNum').val('');
         $('#jTitle').val('');
         $('#aSalary').val('');
+        sum = 0;  //reset global sum to zero on each pass through
+
+
     }); //end clickhandler
+
+    //create remove button click handler
+
+    $('#employeeTable').on('click', $('#removeButton'), function (event) {
+        removeEmployee(event.targ)
+    }; //end remove button
+
+
 } //end onReady
 
-
-$('monthlySum').append(parseFloat(grandTotal));
 //add inputs to table
 function addEmployee(firstName, lastName, idNumber, jobTitle, annSalary) { //create function to append to list
     $('#employeeTable').append(`
@@ -57,11 +80,28 @@ function addEmployee(firstName, lastName, idNumber, jobTitle, annSalary) { //cre
             ${jobTitle}
             </td>
             <td>
-            ${annSalary}
+            $${annSalary}
             </td>
-            <hr>
+            <td>
+            <button id="removeButton">Remove</button>
         </tr>`)
 }; //end add employee
 
+//append running sum divided by 12 to get monthly costs   
+//need to change color to red if monthly costs exceed 20K
+function addSum(sum) {
+    let monTotal = sum / 12;
+    $('#monthlySum').text(parseFloat(monTotal.toFixed(2)));
+    if (monTotal > 20000) {
+        $('#monthlySum').addClass("inTheRed").text(parseFloat(monTotal.toFixed(2)));
+    }
+
+};
+
+
+// sum += annSalary;
+// console.log(sum);
+
+// $('#monthlySum').append(sum);
 
 $(onReady);
