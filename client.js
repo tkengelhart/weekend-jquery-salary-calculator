5c4c11c433b3b355715a51327c47015e148daa97
Tanya Engelhart <tkengelhart@gmail.com>
2021-04-25 01:16:37 -0500

    working on total

--- client.js
+++ client.js
@@ -1,6 +1,4 @@
-
 const emplArr = [];
-let monthlyTotal = 0;
 
 function onReady() {
     // function that runs when onReady is called
@@ -9,31 +7,40 @@ function onReady() {
         const lastName = $('#lName').val(); //variable for last name input
         const idNumber = $('#idNum').val(); //variable for id input
         const jobTitle = $('#jTitle').val(); //variable for job title input
-        const annSalary = ($('#aSalary').val()); //variable for salary input
+        const annSalary = $('#aSalary').val(); //variable for salary input
+
         const emplObj = {  //create employee object
             first: firstName,
             last: lastName,
             id: idNumber,
             title: jobTitle,
             salary: annSalary,
-        };
+        }; //end create object
+
         emplArr.push(emplObj);  //push employee object to array
-        addEmployee(firstName, lastName, idNumber, jobTitle, annSalary)
-        //call add employee function to add employee to list
 
         $('#employeeTable').empty();  //clear employees on initialize
 
         for (let i = 0; i < emplArr.length; i++) {
+            let sum = 0;
+            const grandTotal = sum += emplArr[i].salary;
             //loop through array and populate employees
             addEmployee(emplArr[i].first, emplArr[i].last, emplArr[i].id, emplArr[i].title,
                 emplArr[i].salary);
-        }
-        $('#monthlySum').append(parseFloat(monthlyTotal));
+        }; //end for loop
 
-    });
-};
+        //empty input fields
+        $('#fName').val('');
+        $('#lName').val('');
+        $('#idNum').val('');
+        $('#jTitle').val('');
+        $('#aSalary').val('');
+    }); //end clickhandler
+} //end onReady
 
 
+$('monthlySum').append(parseFloat(grandTotal));
+//add inputs to table
 function addEmployee(firstName, lastName, idNumber, jobTitle, annSalary) { //create function to append to list
     $('#employeeTable').append(`
         <tr>
@@ -54,7 +61,7 @@ function addEmployee(firstName, lastName, idNumber, jobTitle, annSalary) { //cre
             </td>
             <hr>
         </tr>`)
-};
+}; //end add employee
 
 
 $(onReady);
